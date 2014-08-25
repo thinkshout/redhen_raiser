001a5d99f832eba994c35f16672c719f1de1144b
Eric Paxton <heypaxton@gmail.com>
2014-08-25 15:33:26 -0700

    Add responsive header and _cafb app object

--- themes/raiser/js/script.js
+++ themes/raiser/js/script.js
@@ -16,10 +16,56 @@
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
+    // Alias jquery
+    _$ = jQuery;
+    
+    // Store viewport width
+    var viewportWidth = function(){
+      var width = _$(window).width();
+      return width;
+    }
+    
+    // App object
+    var _cafb = {
+      isLarge: function(){
+        return viewportWidth() >= 768  ? true : false;
+      },
 
-    // Place your code here.
+      toggleMenu: function(){
+        _$('#menu-toggle').click(function(e){
+          console.log(viewportWidth());          
+          if (_cafb.isLarge() == false){
+            e.preventDefault();
+            _$('#secondary-menu').slideToggle();
+          }
+        });
+      },
 
-  }
+      setMenuState: function(){
+        var menu = _$('#secondary-menu');
+        if(_cafb.isLarge()){
+          menu.fadeIn();
+        } else {
+          menu.hide();
+        } 
+      },
+
+      init: function() {
+        if (_$('#menu-toggle').length > 0) {
+          this.toggleMenu(); 
+        }
+      }
+    } // end _cafb
+    
+    // Initializer for app object
+    _cafb.init();
+    
+    // Resize listener
+    $(window).resize(function(){
+      _cafb.setMenuState();  
+    });
+    
+  } // end attach
 };
 
 

de80dabab40931698ea8e8b7f732858a5871a8c3
Eric Paxton <heypaxton@gmail.com>
2014-09-22 16:40:22 -0700

    Fix positioning on sidebar

--- themes/raiser/templates/page.tpl.php
+++ themes/raiser/templates/page.tpl.php
@@ -45,37 +45,38 @@
 
     <div id="content" class="column" role="main">
       <?php print render($page['highlighted']); ?>
-      <!-- <?php print $breadcrumb; ?> -->
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h2 class="page__title title" id="page-title"><?php print $title; ?></h2>
+      <div class="main-content-wrapper">
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h2 class="page__title title" id="page-title"><?php print $title; ?></h2>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php if (drupal_is_front_page()): unset($page['content']['system_main']['default_message']); endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+
+        <?php
+          // Render the sidebars to see if there's anything in them.
+          $sidebar_first  = render($page['sidebar_first']);
+          $sidebar_second = render($page['sidebar_second']);
+        ?>
+      </div>
+
+      <?php if ($sidebar_first || $sidebar_second): ?>
+        <aside class="sidebar-first">
+          <?php print $sidebar_first; ?>
+        </aside>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php if (drupal_is_front_page()): unset($page['content']['system_main']['default_message']); endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
     </div>
 
 
-    <?php
-      // Render the sidebars to see if there's anything in them.
-      $sidebar_first  = render($page['sidebar_first']);
-      $sidebar_second = render($page['sidebar_second']);
-    ?>
-
-    <?php if ($sidebar_first || $sidebar_second): ?>
-      <aside class="sidebars">
-        <?php print $sidebar_first; ?>
-        <?php print $sidebar_second; ?>
-      </aside>
-    <?php endif; ?>
 
     <?php if($page['content_secondary']): ?>
       <div id="content-secondary">

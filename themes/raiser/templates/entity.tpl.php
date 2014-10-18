921aa69b3ba062a9c30298e79e632f203f790361
David Lanier <david@thinkshout.com>
2014-10-17 17:41:14 -0700

    Removed title from entity teaser displays. [14619158]

--- themes/raiser/templates/entity.tpl.php
+++ themes/raiser/templates/entity.tpl.php
@@ -29,17 +29,6 @@
  */
 ?>
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>>
-      <?php if ($url): ?>
-        <a href="<?php print $url; ?>"><?php print $title; ?></a>
-      <?php else: ?>
-        <?php print $title; ?>
-      <?php endif; ?>
-    </h2>
-  <?php endif; ?>
-
   <div class="content"<?php print $content_attributes; ?>>
     <?php
       print render($content);

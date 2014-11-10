16737e970ef7b48f0c37a9d15ebccc9e299e50b0
Josef Komenda <jkomenda@Josefs-MacBook-Pro.local>
2014-11-10 15:01:15 -0800

    Use older event delegation for jQuery 1.4.4

--- themes/raiser/js/script.js
+++ themes/raiser/js/script.js
@@ -90,7 +90,7 @@ Drupal.behaviors.my_custom_behavior = {
       noCommas: function() {
         $("input[id^=edit-redhen-campaign-goal-und-0-value]").each(function(){
             this.value=this.value.replace(/,/g, "");
-        }).on('keyup', function(){
+        }).live('keyup', function(){
             this.value=this.value.replace(/,/g, "");
         });
       },
